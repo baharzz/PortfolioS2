18f08ad36b2a222775c4847aa9cfe418e1cffbdb
I394107 <baharzoubin@hotmail.com>
2020-01-16 00:12:49 +0100

    Performance issue verholpen

--- README.md
+++
@@ -1,4 +0,0 @@
-# PortfolioS2
-Portfolio Semester 2 ICT en Media Design
-
-test 
---
+++ js/sprint2.js
@@ -0,0 +1,61 @@
+var modal = document.getElementById('poster1');
+var modal1 = document.getElementById('poster2');
+var modal2 = document.getElementById('poster3');
+var modal3 = document.getElementById('poster4');
+var modal4 = document.getElementById('poster5');
+var modal5 = document.getElementById('poster6');
+var modal6 = document.getElementById('poster7');
+var modal7 = document.getElementById('poster8');
+var modal8 = document.getElementById('poster9');
+var modal9 = document.getElementById('poster10');
+var modal10 = document.getElementById('dried');
+var modal11 = document.getElementById('dried1');
+var modal12 = document.getElementById('persona');
+var modal13 = document.getElementById('storyboard');
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+    if (event.target == modal1) {
+        modal1.style.display = "none";
+    }
+    if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
+    if (event.target == modal3) {
+        modal3.style.display = "none";
+    }
+    if (event.target == modal4) {
+        modal4.style.display = "none";
+    }
+    if (event.target == modal5) {
+        modal5.style.display = "none";
+    }
+    if (event.target == modal6) {
+        modal6.style.display = "none";
+    }
+    if (event.target == modal7) {
+        modal7.style.display = "none";
+    }
+    if (event.target == modal8) {
+        modal8.style.display = "none";
+    }
+    if (event.target == modal9) {
+        modal9.style.display = "none";
+    }
+    if (event.target == modal10) {
+        modal10.style.display = "none";
+    }
+    if (event.target == modal11) {
+        modal11.style.display = "none";
+    }
+    if (event.target == modal12) {
+        modal12.style.display = "none";
+    }
+    if (event.target == modal13) {
+        modal13.style.display = "none";
+    }
+
+
+}
